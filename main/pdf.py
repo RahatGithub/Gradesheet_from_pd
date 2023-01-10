9fdd668beb42fff8241bb9c4dd4e34611531fdce
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2023-01-10 13:39:12 +0600

    PDF view and download is ok

--- main/pdf.py
+++ main/pdf.py
@@ -1,9 +1,7 @@
 from io import BytesIO
-from django.shortcuts import render 
 from django.http import HttpResponse
 from django.template.loader import get_template
 from xhtml2pdf import pisa 
-from django.views import View
 from .models import GradeSheet
 import json
 
@@ -16,6 +14,7 @@ def render_to_pdf(template_src, context_dict={}):
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type="application/pdf")
     return None
+
 
 
 def generate_pdf(request, institute, department, session, reg_no):  
@@ -59,7 +58,7 @@ def generate_pdf(request, institute, department, session, reg_no):
         context = {'gradesheet' : gradesheet, 
                    'session' : session,
                    'reg_no' : reg_no,
-                   'student_name' : name }
+                   'name' : name }
         
         pdf = render_to_pdf('main/gradesheet_view.html', context)
         
@@ -68,11 +67,70 @@ def generate_pdf(request, institute, department, session, reg_no):
             content = "inline; filename=GradeSheet.pdf"
             response['Content-Disposition'] = content 
             return response 
+        
         return HttpResponse("Not found")
     
     except:
         return HttpResponse("Not found") 
 
+
+
+def download_pdf(request, institute, department, session, reg_no):
+    try:
+        institute = institute.replace("_", " ")    # replacing all '_' with <space>
+        department = department.replace("_", " ")  # replacing all '_' with <space>
+        session = session.replace("_", " ")        # replacing all '_' with <space>
+        student_record = GradeSheet.objects.get(institute=institute, department=department, session=session, reg_no=reg_no)
+        results = json.loads(student_record.results)
+        name = student_record.name
+        
+        gradesheet = list()
+        cumulative_credits, cumulative_point = float(0), float(0)   # for the overall result of all semesters
+        for semester_result in results:
+            a_semester = dict()
+            this_semester_credits, this_semester_point = float(0), float(0)   # for only a particular semester
+            course_results = list()
+            for cour in semester_result:
+                GP = float(cour[3])
+                LG = calculate_LG(GP)
+                cour.append(LG)
+                if GP >= 2:            # checking if the obtained GP >= 2 
+                    this_semester_credits += float(cour[2])  # cumulative_credits += course_credits
+                    this_semester_point += GP * float(cour[2])  # cumulative_point += GP * course_credits 
+                course_results.append(cour) 
+                
+            a_semester['course_results'] = course_results
+            a_semester['this_semester_credits'] = this_semester_credits
+            this_semester_GP = round((this_semester_point/this_semester_credits), 2)
+            a_semester['this_semester_GP'] = this_semester_GP
+            a_semester['this_semester_LG'] = calculate_LG(this_semester_GP)
+            cumulative_credits += this_semester_credits
+            a_semester['cumulative_credits'] = cumulative_credits
+            cumulative_point += this_semester_point
+            cumulative_GP = round((cumulative_point/cumulative_credits), 2)
+            a_semester['cumulative_GP'] = cumulative_GP
+            a_semester['cumulative_LG'] = calculate_LG(cumulative_GP)
+            
+            gradesheet.append(a_semester)
+            
+        context = {'gradesheet' : gradesheet, 
+                'session' : session,
+                'reg_no' : reg_no,
+                'name' : name }
+        
+        pdf = render_to_pdf('main/gradesheet_view.html', context)
+        
+        if pdf:
+            response = HttpResponse(pdf, content_type="application/pdf")
+            # content = "attachment; filename=.pdf"
+            content = "attachment; filename=%s.pdf" %(reg_no)
+            response['Content-Disposition'] = content 
+            return response 
+        
+        return HttpResponse("Not found")
+    
+    except:
+        return HttpResponse("Not found")
 
 
 def calculate_LG(GP):
--- main/urls.py
+++ main/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from .pdf import generate_pdf
+from .pdf import generate_pdf, download_pdf
 
 urlpatterns = [
     path('', views.upload, name='index'), 
     path('batch_view/<str:institute>/<str:department>/<str:session>/', views.batch_view, name='batch_view'),
-    path('gradesheet_view/<str:institute>/<str:department>/<str:session>/<str:reg_no>/', generate_pdf, name="generate_pdf"), 
-    path('test/<str:institute>/<str:department>/<str:session>/<str:reg_no>/', views.test, name='test'), 
+    # path('gradesheet_view/<str:institute>/<str:department>/<str:session>/<str:reg_no>/', generate_pdf, name="generate_pdf"), 
+    # path('test/<str:institute>/<str:department>/<str:session>/<str:reg_no>/', views.test, name='test'), 
+    path('gradesheet/<str:institute>/<str:department>/<str:session>/<str:reg_no>/', generate_pdf, name="view_pdf"),
+    path('download_gradesheet/<str:institute>/<str:department>/<str:session>/<str:reg_no>/', download_pdf, name="download_pdf")
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
