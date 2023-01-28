a453db71dec5d88fad580a65ff600616af0d990e
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2023-01-29 00:48:04 +0600

    again commit

--- main/views.py
+++ main/views.py
@@ -136,13 +136,201 @@ def batch_view(request, institute, department, session):
 
 
 
-# def gradesheet_view(request, institute, department, session, reg_no):
+
+def calculate_LG(GP):
+    if GP == 4.00 : LG = "A+"
+    elif GP >= 3.75 : LG = "A"
+    elif GP >= 3.50 : LG = "A-" 
+    elif GP >= 3.25 : LG = "B+"
+    elif GP >= 3.00 : LG = "B"
+    elif GP >= 2.75 : LG = "B-"
+    elif GP >= 2.50 : LG = "C+"
+    elif GP >= 2.25 : LG = "C"
+    elif GP >= 2.00 : LG = "C-" 
+    else: LG = "F"
+    return LG 
+
+
+def gradesheet_view(request, institute, department, session, reg_no):
+    try:
+        institute = institute.replace("_", " ")    # replacing all '_' with <space>
+        department = department.replace("_", " ")  # replacing all '_' with <space>
+        session = session.replace("_", " ")        # replacing all '_' with <space>
+        student_record = GradeSheet.objects.get(institute=institute, department=department, session=session, reg_no=reg_no)
+        results = json.loads(student_record.results)
+        name = student_record.name
+        
+        gradesheet = list()
+        cumulative_credits, cumulative_point = float(0), float(0)   # for the overall result of all semesters
+        for semester_result in results:
+            a_semester = dict()
+            this_semester_credits, this_semester_point = float(0), float(0)   # for only a particular semester
+            course_results = list()
+            for cour in semester_result:
+                GP = float(cour[3])
+                LG = calculate_LG(GP)
+                cour.append(LG)
+                if GP >= 2:            # checking if the obtained GP >= 2 
+                    this_semester_credits += float(cour[2])  # cumulative_credits += course_credits
+                    this_semester_point += GP * float(cour[2])  # cumulative_point += GP * course_credits 
+                course_results.append(cour) 
+                
+            a_semester['course_results'] = course_results
+            a_semester['this_semester_credits'] = this_semester_credits
+            this_semester_GP = round((this_semester_point/this_semester_credits), 2)
+            a_semester['this_semester_GP'] = this_semester_GP
+            a_semester['this_semester_LG'] = calculate_LG(this_semester_GP)
+            cumulative_credits += this_semester_credits
+            a_semester['cumulative_credits'] = cumulative_credits
+            cumulative_point += this_semester_point
+            cumulative_GP = round((cumulative_point/cumulative_credits), 2)
+            a_semester['cumulative_GP'] = cumulative_GP
+            a_semester['cumulative_LG'] = calculate_LG(cumulative_GP)
+            
+            gradesheet.append(a_semester)
+
+        # context = {'gradesheet' : gradesheet, 
+        #            'session' : session,
+        #            'reg_no' : reg_no,
+        #            'name' : name }
+
+        return HttpResponse(gradesheet)
+
     
-#     context = {'institute' : institute,
-#                'department' : department,
-#                'session' : session,
-#                'reg_no' : reg_no }
-#     return render(request, "main/gradesheet_view.html", context)
+    except:
+        return HttpResponse("Not found...$") 
+    
+    # ################################################
+
+
+
+    # student = Student.objects.get(reg_no=reg_no)
+    # session = student.session
+    # batch = Batch.objects.get(session=session)
+    # batch_no = batch.batch_no
+    # student = Student.objects.get(reg_no=reg_no)
+    # student_name = student.name
+
+    # gradesheet = list()
+    # semester_results = Result.objects.filter(reg_no=reg_no)
+    # cumulative_credits, cumulative_point, cumulative_LG = float(
+    #     0), float(0), 'NA'   # for the overall result of all semesters
+    # inc = 1
+    # for result in semester_results:
+    #     a_semester = dict()
+    #     course_results = json.loads(result.course_results)
+    #     course_results_info = list()
+    #     this_semester_credits, this_semester_point, this_semester_LG = float(
+    #         0), float(0), 'NA'   # for only a particular semester
+    #     for cour in course_results.items():
+    #         course_info = list()
+    #         # pushing the course_code at index=0
+    #         course_info.append(cour[0])
+    #         course = Course.objects.filter(
+    #             batch_no=batch_no, course_code=cour[0]).first()
+    #         # pushing the course_title at index=1
+    #         course_info.append(course.course_title)
+    #         # pushing the course_credits at index=2
+    #         course_info.append(course.course_credits)
+    #         # pushing the obtained GP at index=3
+    #         course_info.append(cour[1]['GP'])
+    #         # pushing the obtained LG at index=4
+    #         course_info.append(cour[1]['LG'])
+    #         # checking if the obtained GP >= 2
+    #         if float(course_info[3]) >= 2:
+    #             # cumulative_credits += course_credits
+    #             this_semester_credits += course_info[2]
+    #             # cumulative_point += GP * course_credits
+    #             this_semester_point += float(course_info[3]) * course_info[2]
+    #         course_results_info.append(course_info)
+
+    #     a_semester['this_semester_name'] = semester_name(inc)
+    #     inc += 1
+    #     a_semester['this_semester_credits'] = this_semester_credits
+    #     a_semester['course_results_info'] = course_results_info
+
+    #     this_semester_GP = round(
+    #         (this_semester_point/this_semester_credits), 2)
+    #     a_semester['this_semester_GP'] = this_semester_GP
+
+    #     if this_semester_GP == 4.00:
+    #         this_semester_LG = "A+"
+    #     elif this_semester_GP >= 3.75:
+    #         this_semester_LG = "A"
+    #     elif this_semester_GP >= 3.50:
+    #         this_semester_LG = "A-"
+    #     elif this_semester_GP >= 3.25:
+    #         this_semester_LG = "B+"
+    #     elif this_semester_GP >= 3.00:
+    #         this_semester_LG = "B"
+    #     elif this_semester_GP >= 2.75:
+    #         this_semester_LG = "B-"
+    #     elif this_semester_GP >= 2.50:
+    #         this_semester_LG = "C+"
+    #     elif this_semester_GP >= 2.25:
+    #         this_semester_LG = "C"
+    #     elif this_semester_GP >= 2.00:
+    #         this_semester_LG = "C-"
+    #     else:
+    #         this_semester_LG = "F"
+    #     a_semester['this_semester_LG'] = this_semester_LG
+
+    #     cumulative_credits += this_semester_credits
+    #     a_semester['cumulative_credits'] = cumulative_credits
+    #     cumulative_point += this_semester_point
+    #     cumulative_GP = round((cumulative_point/cumulative_credits), 2)
+    #     a_semester['cumulative_GP'] = cumulative_GP
+
+    #     if cumulative_GP == 4.00:
+    #         cumulative_LG = "A+"
+    #     elif cumulative_GP >= 3.75:
+    #         cumulative_LG = "A"
+    #     elif cumulative_GP >= 3.50:
+    #         cumulative_LG = "A-"
+    #     elif cumulative_GP >= 3.25:
+    #         cumulative_LG = "B+"
+    #     elif cumulative_GP >= 3.00:
+    #         cumulative_LG = "B"
+    #     elif cumulative_GP >= 2.75:
+    #         cumulative_LG = "B-"
+    #     elif cumulative_GP >= 2.50:
+    #         cumulative_LG = "C+"
+    #     elif cumulative_GP >= 2.25:
+    #         cumulative_LG = "C"
+    #     elif cumulative_GP >= 2.00:
+    #         cumulative_LG = "C-"
+    #     else:
+    #         cumulative_LG = "F"
+    #     a_semester['cumulative_LG'] = cumulative_LG
+
+    #     gradesheet.append(a_semester)
+
+    # # for gs in gradesheet:
+    # #     print(gs, end="\n\n")
+
+    # li = []
+    # a = []
+    # for i in range(len(gradesheet)):
+    #     if i % 2 == 0:
+    #         a = gradesheet[i]
+    #     else:
+    #         try:
+    #             b = []
+    #             b.append(a)
+    #             b.append(gradesheet[i])
+    #             li.append(b)
+    #         except:
+    #             li.append(a)
+    # if len(gradesheet) % 2 == 1:
+    #     b = []
+    #     b.append(a)
+    #     li.append(b)
+
+    # for couple in li:
+    #     for single in couple:
+    #         print(single)
+
+    # return render(request, 'main/gradesheet_view.html', {'session': session, 'reg_no': reg_no, 'student_name': student_name, 'gradesheet': gradesheet, 'collection': li})
 
 
 
